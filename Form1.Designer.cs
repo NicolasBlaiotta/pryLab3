bdc8ec1284996e78689dbd69e86109b6d129cd6b
NicolasBlaiotta <nicolasvilas1962@gmail.com>
2024-04-07 16:22:01 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace pryLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblFirmar = new System.Windows.Forms.Label();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(67, 88);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 311);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblFirmar
+            // 
+            this.lblFirmar.AutoSize = true;
+            this.lblFirmar.Location = new System.Drawing.Point(67, 38);
+            this.lblFirmar.Name = "lblFirmar";
+            this.lblFirmar.Size = new System.Drawing.Size(38, 13);
+            this.lblFirmar.TabIndex = 1;
+            this.lblFirmar.Text = "Firmar:";
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.Location = new System.Drawing.Point(458, 220);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.BtnLimpiar.TabIndex = 2;
+            this.BtnLimpiar.Text = "Limpiar";
+            this.BtnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnLimpiar);
+            this.Controls.Add(this.lblFirmar);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblFirmar;
+        private System.Windows.Forms.Button BtnLimpiar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryLab3
+{
+    public partial class Form1 : Form
+    {
+        private Point previousPoint;
+        private Pen pen;
+        public Form1()
+        {
+            InitializeComponent();
+            pen = new Pen(Color.Black, 2); // Color y grosor del trazo
+            pictureBox1.BackColor = Color.White; // Color de fondo del PictureBox
+            pictureBox1.Paint += PictureBox1_Paint;
+            pictureBox1.MouseDown += PictureBox1_MouseDown;
+            pictureBox1.MouseMove += PictureBox1_MouseMove;
+            pictureBox1.MouseUp += PictureBox1_MouseUp;
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            using (var g = e.Graphics)
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            }
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            previousPoint = e.Location;
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (previousPoint != null && e.Button == MouseButtons.Left)
+            {
+                using (Graphics g = pictureBox1.CreateGraphics())
+                {
+                    g.DrawLine(pen, previousPoint.Value, e.Location);
+                }
+                previousPoint = e.Location;
+            }
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            previousPoint = null;
+        }
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh(); // Limpia la firma
+        }
+    }
+}
+
